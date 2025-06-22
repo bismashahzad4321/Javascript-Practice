a48aa7766e86e977642fb257696acde96d6755c6
bismashahzad4321 <bismasheikh5599@gmail.com>
2025-06-22 14:18:37 +0500

    string to number

--- Lecture 21 Assignment/calculator.js
+++ Lecture 21 Assignment/calculator.js
@@ -9,22 +9,31 @@
 let age1 = prompt("Enter Your First Number");
 let age2 = prompt("Enter Your Second Numebr");
 let userWant = prompt("What You Want");
-// let add = age1 + age2;
+// age1 = Number(prompt("Enter Your First Number"));
+// age2 = Number(prompt("Enter Your Second Number"));
+// let userAdd = Number(age1 + age2);
+let userAdd1=Number(age1);
+let userAdd2=Number(age2);
+addition=userAdd1+userAdd2;
 if (userWant === "add") {
-    console.log( age1 + age2);
+    console.log(addition);
 }
-    else if(userWant==="subtract"){
-        console.log( age1 - age2);
-    }
-     else if(userWant ==="multipy"){
-        console.log( age1 * age2);
-    }
-     else if(userWant==="divide"){
-        console.log( age1 / age2);
-    }
-     else if(userWant ==="exponent"){
-        console.log( age1 ** age2);
-    }
-    else (console.log("Sorry Man"));
+else if (userWant === "subtract") {
+    console.log(age1 - age2);
+}
+else if (userWant === "multipy") {
+    console.log(age1 * age2);
+}
+else if (userWant === "divide") {
+    console.log(age1 / age2);
+}
+else if (userWant === "exponent") {
+    console.log(age1 ** age2);
+}
+else (console.log("Sorry Man"));
 
 
+let str="123";
+console.log(typeof(str));
+let strtonumber=Number(str);
+console.log(strtonumber);
