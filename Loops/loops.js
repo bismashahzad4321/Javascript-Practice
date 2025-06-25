8b4fdc1418f38bbf6715734bbfa0412a330e557c
bismashahzad4321 <bismasheikh5599@gmail.com>
2025-06-25 23:00:18 +0500

    LOOPS

---
+++ Loops/loops.js
@@ -0,0 +1,18 @@
+
+// Practice Tasks / Quiz
+// 1.
+// Use a while loop to print numbers from 10 to 1.
+// 2.
+// Use a do-while loop to keep asking the user for a number until they enter 0.
+// 3.
+// Print a square of # symbols (5x5) using nested loops.
+// 4.
+// Sum all even numbers from 1 to 50 using a for loop.
+
+
+// Use a while loop to print numbers from 10 to 1.
+
+let a=10;
+for(let i=0; i<=20; i++){
+    console.log(i);
+}
